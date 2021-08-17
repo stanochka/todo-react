3eeb0e20b29b67eb44b2d8de826630d61a94cb8a
stanochka <krozzochka@gmail.com>
2021-08-17 16:47:47 +0300

    Add style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
 const Header = () => {
+  const headerStyle = {
+    padding: "20px 0",
+    lineHeight: "1.5em",
+  }
   return (
     <header>
       <h1>Todo List</h1>
--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -65,12 +65,14 @@ class TodoContainer extends React.Component {
 
   render() {
     return (
-      <div>
-        <Header />
-        <TodoInput addTodoProps={this.addTodo} />
-        <TodosList todos={this.state.todos}
-                   handleChangeProps={this.handleChange}
-                   deleteTodoProps={this.delTodo} />
+      <div className="container">
+        <div className="inner">
+          <Header />
+          <TodoInput addTodoProps={this.addTodo} />
+          <TodosList todos={this.state.todos}
+                     handleChangeProps={this.handleChange}
+                     deleteTodoProps={this.delTodo} />
+        </div>
       </div>
     )
   }
